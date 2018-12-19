3803192a9345aa58b154561069a619848c8e8f7d
Yansen XU <xys8804@gmail.com>
2018-12-19 12:35:38 +0900

    test function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,31 @@
+
+path = 'C:\Users\Yansen XU\Documents\programs\skype_trace\skype_e2e_trace'
+import matplotlib.pyplot as plt
+# "No.","Time","Source","Destination","Protocol","Length","Info"
+
+src_ip_list = []
+def test():
+    with open(path, 'r') as trace_file:
+        line = trace_file.readline()
+        print line
+        while line:
+            line = trace_file.readline()
+            # print float(line.split(',')[2][1:-1])
+            src_ip = line.split(',')[2][1:-1]
+            if src_ip not in src_ip_list:
+                src_ip_list.append(src_ip)
+                print src_ip
+
+
+
+
+
+
+def main():
+    test()
+
+
+
+
 if __name__ == '__main__':
-    print 'hello word'
+    main()
