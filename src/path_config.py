6f9518af92d30eb5df0d0ef7878297b8fac0821f
YANSEN.XU <xys8804@gmail.com>
2019-01-14 23:50:32 +0900

    flow file path config for mac

--- src/path_config.py
+++ src/path_config.py
@@ -1,2 +1,3 @@
 import os
-flow_file_path = os.path.dirname(os.path.abspath(__file__)) + '\\..\\datasets\\flow_data\\'
+# flow_file_path = os.path.dirname(os.path.abspath(__file__)) + '\\..\\datasets\\flow_data\\'
+flow_file_path = os.path.dirname(os.path.abspath(__file__)) + '/../datasets/flow_data/'
