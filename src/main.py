0d7e548375d38f10e72638e8a1a42b79ca5cc920
Yansen XU <xys8804@gmail.com>
2019-02-08 19:33:41 +0900

    processing capacity test

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-import preprecess_data
 import os
 import path_config
+from processing_capacity import processing_capacity
+
 
 flow_file_path = path_config.flow_file_path
 
@@ -11,8 +12,9 @@ flow_file_path = path_config.flow_file_path
 def traffic_rate_data(file_name, interval=1):
     """Read traffic data and calculate traffic rate in interval.
     """
-    x = []
-    y = []
+    time_slot = []
+    traff_rate = []
+    packet_length = []
     with open(flow_file_path + file_name, 'r') as flow:
         line = flow.readline()
         l = line.split(',')
@@ -26,24 +28,38 @@ def traffic_rate_data(file_name, interval=1):
             time = float(l[1])
             length = int(l[4])
             if (time - base_time) >= interval:
-                x.append(t)
-                y.append(sum_length/(time-base_time))
+                time_slot.append(t)
+                traff_rate.append(sum_length/(time-base_time))
+                packet_length.append(sum_length)
                 base_time = time
                 sum_length = 0
                 t = t + 1
             else:
                 sum_length = sum_length + length
             line = flow.readline()
-    return x, y
+    return time_slot, traff_rate, packet_length
+
+
+def traffic_rate_data_test():
+    [time_slot_1, traff_rate_1, packet_length_1] = traffic_rate_data('192.0.96.1-128.0.0.128.csv', 300)
+    [time_slot_2, traff_rate_2, packet_length_2] = traffic_rate_data('1.0.0.2-128.0.0.2.csv', 10)
+    time_slot_2 = [x * 10 for x in time_slot_2]
+    # plt.plot(time_slot_1, traff_rate_1, 'r-', time_slot_2, traff_rate_2, 'b-')
+    plt.plot(time_slot_1, traff_rate_1)
+    plt.legend()
+    plt.show()
+
+def processing_capacity_test():
+    capacity = processing_capacity(100, 200, 1024)
+    print(capacity)
 
 
 def main():
-    [x1, y1] = traffic_rate_data('1.0.0.2-128.0.0.2.csv', 1)
-    [x2, y2] = traffic_rate_data('1.0.0.2-128.0.0.2.csv', 10)
-    x2 = [x * 10 for x in x2]
-    plt.plot(x1, y1, 'r-', x2, y2, 'b-')
-    plt.legend()
-    plt.show()
+    # processing_capacity_test()
+    traffic_rate_data_test()
+    
+
+
 
 
 if __name__ == '__main__':
---
+++ src/processing_capacity.py
@@ -0,0 +1,2 @@
+def processing_capacity(flow_rate, packet_processing_time, packet_length):
+    return flow_rate * packet_processing_time / packet_length
